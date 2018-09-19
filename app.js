246a60cc82e7d40dda6142e162b49f0eafbfb1ca
irham dzuhri <irhamdz@gmail.com>
2018-09-19 09:20:10 +0700

    require http-errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const createError = require('http-errors');
 
 const indexRouter = require('./routes/index');
 
